1a90e06b1c25f345a8d9a906b65e56a73a33b91c
jensbunning86 <94413748+jensbunning86@users.noreply.github.com>
2022-01-03 19:56:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let name = 'age';
+let age = 'vijfendertig';
+console.log(name)
+
+let x = 4;
+let y = 4;
+let z = x + y;
+
+let q = 3;
+let w = 2;
+let e = q - w;
+
+let r = 6;
+let t = 9;
+let i = r * t;
+
+let p = 70;
+let a = 10;
+let m = p / a;
+
+let f = 8;
+let s = 80;
+let h = f %= s;
+
+
